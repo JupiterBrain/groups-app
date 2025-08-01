59d64e9800590851ff24293565f50ca16b8ad411
JupiterBrain <mueller.michael2006@gmail.com>
2025-08-01 16:52:31 +0200

    Updated InpuCard Widget Model Design

--- lib/models/input_card.dart
+++ lib/models/input_card.dart
@@ -8,7 +8,7 @@ class InputCard extends StatelessWidget {
   final Spreadsheet? table;
   final List<String> errors;
 
-  InputCard(this.titlePrefix, this.pasteAction, this.table, this.errors,
+  const InputCard(this.titlePrefix, this.pasteAction, this.table, this.errors,
       {super.key});
 
   @override
@@ -17,18 +17,38 @@ class InputCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Wrap(
+              spacing: 20,
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              alignment: WrapAlignment.spaceBetween,
+              runSpacing: 8,
               children: [
                 Text(
                   '${titlePrefix}daten',
                   style: Theme.of(context).textTheme.titleMedium,
                 ),
-                ElevatedButton(
-                  onPressed: pasteAction,
-                  child: const Text('Paste'),
+                FittedBox(
+                  child: Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: pasteAction,
+                        child: const Row(
+                          children: [
+                            Icon(Icons.paste),
+                            SizedBox(width: 8),
+                            Text('Einfügen'),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        tooltip: "Show format info",
+                        onPressed: () => {},
+                        icon: const Icon(Icons.info),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
