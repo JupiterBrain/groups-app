b91684b4960c1549b58a1af22ce7df24a7f71570
JupiterBrain <mueller.michael2006@gmail.com>
2025-08-01 16:51:01 +0200

    Implemented parsing logic

--- lib/algorithm/parser.dart
+++ lib/algorithm/parser.dart
@@ -1,20 +1,129 @@
 import 'package:groups_v4/algorithm/classes.dart';
 import 'package:groups_v4/utils.dart';
 
-class Parser {
-  bool allowDuplicates = false;
-  bool allowEmpty = false;
-  bool allowExcess = false;
+Result<(List<Group>, List<Item>), (List<String>, List<String>)> parse(
+  int nrOfChoices,
+  TRows groupsTable,
+  TRows itemsTable, {
+  bool allowDuplicates = false,
+  bool allowEmpty = false,
+  bool allowExcess = false,
+}) {
+  var (groups, groupsErrors) = parseGroups(groupsTable);
+  var (items, itemsErrors) = parseItems(nrOfChoices, itemsTable, groups,
+      allowDuplicates: allowDuplicates, allowEmpty: allowEmpty);
 
-  int nrOfChoices = 3;
-  int defaultCapacity = -1;
+  var totalCapacity = groups.values
+      .map(
+        (group) => group.capacity,
+      )
+      .reduce(
+        (sum, c) => sum + c,
+      );
 
-  late TRows groupsTable;
-  late TRows itemsTable;
+  if (totalCapacity < items.length && !allowExcess) {
+    groupsErrors.insert(0, "Not enough capacity");
+  }
 
-  late List<String> groupParsingErrors = [];
-  late List<String> itemParsingErrors = [];
+  if (groupsErrors.isNotEmpty || itemsErrors.isNotEmpty) {
+    return Result.error((groupsErrors, itemsErrors));
+  } else {
+    return Result.ok((groups.values.toList(), items));
+  }
+}
 
-  late Map<String, Group> groups;
-  late List<Item> items = [];
+(Map<String, Group>, List<String>) parseGroups(TRows groupsTable) {
+  List<String> groupsErrors = [];
+  Map<String, Group> groups = {};
+
+  void error(String error) {
+    groupsErrors.add(error);
+  }
+
+  void parseGroup(List<String> row, int id) {
+    if (row.length < 2) {
+      return error("Error in row $id: A group must have columns for at least "
+          "an identifier and a capacity.");
+    }
+
+    var identifier = row.first;
+
+    if (groups.containsKey(identifier)) {
+      return error("Error in row $id: A groups identifier must be unique.");
+    }
+
+    var capacity = int.tryParse(row.last);
+    if (capacity == null) {
+      return error("Error in row $id: '${row.last}' is not a valid integer.");
+    }
+
+    var description = row.sublist(2, row.length - 1);
+
+    groups[identifier] = Group(id, identifier, description, capacity);
+  }
+
+  for (int i = 0; i < groupsTable.length; i++) {
+    var row = groupsTable[i];
+    parseGroup(row, i + 1);
+  }
+
+  return (groups, groupsErrors);
+}
+
+(List<Item>, List<String>) parseItems(
+    int nrOfChoices, TRows itemsTable, Map<String, Group> groups,
+    {bool allowDuplicates = false, bool allowEmpty = false}) {
+  List<String> itemsErrors = [];
+  List<Item> items = [];
+
+  void error(String error) {
+    itemsErrors.add(error);
+  }
+
+  void parseItem(List<String> row, int id) {
+    if (row.length < 1 + nrOfChoices) {
+      return error(
+          "Error in row $id: Element must have columns for all choices.");
+    }
+
+    var description = row.sublist(1, row.length - nrOfChoices);
+
+    var choices = <Group>[];
+
+    for (int k = 0; k < nrOfChoices; k++) {
+      var identifier = row[row.length - nrOfChoices + k];
+
+      if (identifier.trim().isEmpty) {
+        if (allowEmpty && k > 0) {
+          choices.add(choices[k - 1]);
+          continue;
+        } else {
+          return error("Error in row $id: choice ${k + 1} is empty.");
+        }
+      }
+
+      var group = groups[identifier];
+
+      if (group == null) {
+        return error("Error in row $id: group '$identifier' does not exist.");
+      }
+      if (choices.contains(group) && !allowDuplicates) {
+        return error("Error in line $id: choice ${k + 1} is a duplicate.");
+      }
+
+      choices.add(group);
+    }
+
+    items.add(Item(id, description, choices));
+  }
+
+  for (int i = 0; i < itemsTable.length; i++) {
+    var row = itemsTable[i];
+    parseItem(row, i + 1);
+  }
+
+  //Randomize order
+  items.shuffle();
+
+  return (items, itemsErrors);
 }
