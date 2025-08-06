f79ff311de160477a8c229d19f0cb0ff10218bad
JupiterBrain <mueller.michael2006@gmail.com>
2025-08-06 11:47:56 +0200

    Adapted input_card design

--- lib/models/input_page/input_card.dart
+++ lib/models/input_page/input_card.dart
@@ -52,13 +52,15 @@ class InputCard extends StatelessWidget {
                 ),
               ],
             ),
+            const SizedBox(height: 8),
             ReactiveWrapper(
               reactiveValues: [table],
               builder: (context) {
                 if (~table == null) {
                   return Container(
+                    padding: const EdgeInsets.all(8),
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.red, width: 2),
+                      border: Border.all(color: Colors.amber, width: 2),
                       borderRadius: const BorderRadius.all(Radius.circular(8)),
                     ),
                     child: Column(
@@ -80,16 +82,36 @@ class InputCard extends StatelessWidget {
             ReactiveWrapper(
               reactiveValues: [errors],
               builder: (context) {
-                return Column(
-                  children: (~errors)
-                      .map(
-                        (error) => Text(
-                          error,
-                          style: const TextStyle(color: Colors.red),
+                if ((~errors).isNotEmpty) {
+                  return ConstrainedBox(
+                    constraints: const BoxConstraints(maxHeight: 200),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.red, width: 2),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(8)),
+                      ),
+                      child: SingleChildScrollView(
+                        child: Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: (~errors)
+                                .map(
+                                  (error) => Text(
+                                    error,
+                                    style: const TextStyle(color: Colors.red),
+                                  ),
+                                )
+                                .toList(),
+                          ),
                         ),
-                      )
-                      .toList(),
-                );
+                      ),
+                    ),
+                  );
+                } else {
+                  return const SizedBox.shrink();
+                }
               },
             ),
             ReactiveWrapper(
